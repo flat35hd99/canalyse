cc579d56548809f8a973ce658177949ad2973e20
Wataru Sugiura <38929316+flat35hd99@users.noreply.github.com>
2021-11-07 02:29:23 +0900

    Update canalyse/validator.py
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- canalyse/validator.py
+++ canalyse/validator.py
@@ -2,10 +2,6 @@ class ConductivityValidator:
     def __init__(self, df) -> None:
         self.df = df
 
-
-
-
-
     def standard_deviation_is_less_than(self, threshold):
         deviation = self.df["conductivity"].std()
         return deviation < threshold
