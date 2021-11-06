ce733f754a738ddf8da1dc65440b0a1016bc6be7
flat35hd99 <flat35hd99@gmail.com>
2021-11-06 17:40:26 +0900

    wip cli

--- canalyse/cli.py
+++ canalyse/cli.py
@@ -1,5 +1,21 @@
+import click
+
+
+@click.group()
 def cli():
     pass
+
+
+@cli.command()
+@click.option("--under", is_flag=True)
+def conductivity(under):
+    if under:
+        click.echo("hogehoge")
+
+
+@cli.command()
+def current():
+    click.echo("fugafuga")
 
 
 if __name__ == "__main__":
