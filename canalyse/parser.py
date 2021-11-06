c0dd60cbb855cf39bc0924545e60d81dd016a6cf
flat35hd99 <flat35hd99@gmail.com>
2021-11-06 17:02:29 +0900

    Add formatter

--- canalyse/parser.py
+++ canalyse/parser.py
@@ -1,4 +1,8 @@
+from numpy import float64
 import pandas as pd
+import re
+
+# from pandas.core.arrays.integer import Int64Dtype
 
 
 class ConductivityParser:
@@ -11,20 +15,17 @@ class ConductivityParser:
             delim_whitespace=True,
             header=None,
             names=self.header,
+            dtype={"source": str, "target": str, "conductivity": float64},
         )
         return df
 
 
-def conductivity_parser():
-    return ConductivityParser
-
-
 def get_number(col):
-    return col.split()[0]
+    return int(re.sub("^0+", "", col.split("_")[0]))
 
 
 def get_residue(col):
-    return col.split()[1]
+    return col.split("_")[1]
 
 
 class ConductivityFormatter:
@@ -34,6 +35,6 @@ class ConductivityFormatter:
     def run(self, df):
         df = df.copy()
         for header in ["source", "target"]:
-            df[f"{header}_numbeer"] = df[header].map(get_number)
+            df[f"{header}_number"] = df[header].map(get_number)
             df[f"{header}_residue"] = df[header].map(get_residue)
         return df
--- test/test_parser.py
+++ test/test_parser.py
@@ -1,17 +1,41 @@
 import unittest
 import os
+import pandas as pd
+from pandas._testing import assert_frame_equal
+from canalyse.parser import ConductivityParser, ConductivityFormatter
 
-# from canalyse.parser import ConductivityParser as conpas
-from canalyse.parser import conductivity_parser
+conductivity_file = (
+    f"{os.path.dirname(os.path.abspath(__file__))}/assets/conductivity.dot"
+)
 
 
 class TestParser(unittest.TestCase):
     def test_conductivity_parser(self):
-        parser = conductivity_parser()
-        reference_file = (
-            f"{os.path.dirname(os.path.abspath(__file__))}/assets/conductivity.dot"
-        )
-        df = parser().run(reference_file)
+        parser = ConductivityParser()
+
+        df = parser.run(conductivity_file)
         # Has header expected
         reference_header = ["source", "target", "conductivity"]
         assert all(df.columns == reference_header)
+
+    def test_conductivity_formatter(self):
+        parser = ConductivityParser()
+        df = parser.run(conductivity_file)
+        formatter = ConductivityFormatter()
+        new_df = formatter.run(df)
+        reference_df = pd.DataFrame(
+            {
+                "source": ["00001_MET", "00001_MET", "00001_MET"],
+                "target": ["00036_PHE", "00005_GLU", "00004_ASP"],
+                "conductivity": [
+                    float(0.0174476321812),
+                    float(0.0214987056692),
+                    float(0.0124389690164),
+                ],
+                "source_number": [1, 1, 1],
+                "source_residue": ["MET", "MET", "MET"],
+                "target_number": [36, 5, 4],
+                "target_residue": ["PHE", "GLU", "ASP"],
+            }
+        )
+        assert_frame_equal(new_df, reference_df)
