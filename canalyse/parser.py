31ab6f28d359bd22ce71507335e4429bec94ef22
flat35hd99 <flat35hd99@gmail.com>
2021-11-11 19:20:47 +0900

    Add volume parser and formatter

--- canalyse/parser.py
+++ canalyse/parser.py
@@ -1,4 +1,4 @@
-from numpy import float64
+from numpy import float64, int64
 import pandas as pd
 import re
 
@@ -37,4 +37,84 @@ class ConductivityFormatter:
         for header in ["source", "target"]:
             df[f"{header}_number"] = df[header].map(get_number)
             df[f"{header}_residue"] = df[header].map(get_residue)
+        return df
+
+
+class BiovoronoiVolumeParser:
+    def __init__(self) -> None:
+        pass
+
+    def run(self, filename):
+        df = pd.read_csv(
+            filename,
+            dtype={
+                "residue_id": int64,
+                "residue_name": str,
+                "atom_serial_number": int64,
+                "volume": float64,
+            },
+        )
+        return df
+
+
+class BiovoronoiVolumeFormatter:
+    def __init__(self):
+        pass
+
+    def run(self, df):
+        df = df.drop(["atom_serial_number"], axis=1)
+        return df
+
+
+class VldpVolumeParser:
+    def __init__(self) -> None:
+        pass
+
+    def run(self, filename):
+        """parse vldp volume
+
+        Args:
+            filename (str): input file
+        """
+        df = pd.read_table(
+            filename,
+            delim_whitespace=True,
+            comment="#",
+            names=[
+                "MOL",
+                "NUMMOL",
+                "ATM",
+                "NUMATM",
+                "X",
+                "Y",
+                "Z",
+                "AREA",
+                "VOLUME",
+                "LOCUS",
+            ],
+            dtype={
+                "MOL": str,
+                "NUMMOL": int64,
+                "ATM": str,
+                "NUMATM": int64,
+                "X": float64,
+                "Y": float64,
+                "Z": float64,
+                "AREA": float64,
+                "VOLUME": float64,
+                "LOCUS": int64,
+            },
+        )
+        return df
+
+
+class VldpVolumeFormatter:
+    def __init__(self) -> None:
+        pass
+
+    def run(self, df):
+        df = df[["MOL", "NUMMOL", "VOLUME"]]
+        df = df.rename(
+            columns={"MOL": "residue_name", "NUMMOL": "residue_id", "VOLUME": "volume"}
+        )
         return df
